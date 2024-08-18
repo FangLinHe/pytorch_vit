b994e0f953970b92782d3fe2114c9bd737816810
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-08-18 17:51:40 +0200

    Update comments

--- src/pytorch_vit/patch_extractor.py
+++ src/pytorch_vit/patch_extractor.py
@@ -19,14 +19,21 @@ class PatchExtractor(nn.Module):
 
         Returns:
             torch.Tensor: sequences of flattened 2D patches with dimension
-                BxNx(P_h * P_w * C), where N = H * W / (P_h * P_w)
+                BxNx(C * P_h * P_w), where N = H * W / (P_h * P_w)
         """
-        axis_moved: torch.Tensor = x.moveaxis(1, 3)  # BCHW to BHWC
-        h_patches_unfolded: torch.Tensor = axis_moved.unfold(1, self.patch_h, self.patch_h)  # Bx(H/P_h)xWxCxP_h
-        patches_unfolded: torch.Tensor = h_patches_unfolded.unfold(
-            2, self.patch_w, self.patch_w
-        )  # Bx(H/P_h)x(W/P_w)xCxP_hxP_w
-        flattend_patches: torch.Tensor = patches_unfolded.flatten(3)  # Bx(H/P_h)x(W/P_w)x(C * P_h * P_w)
-        patches: torch.Tensor = flattend_patches.flatten(1, 2)  # Bx(H/P_h)x(W/P_w)x(C * P_h * P_w)
+        # Move dim=1 to dim=3: dim(B, C, H, W) --> dim(B, H, W, C)
+        axis_moved: torch.Tensor = x.moveaxis(1, 3)
+
+        # Extract patches along dim=1: dim(B, H, W, C) --> dim(B, H / P_h, W, C, P_h)
+        h_patches_unfolded: torch.Tensor = axis_moved.unfold(1, self.patch_h, self.patch_h)
+
+        # Extract patches along dim=2: dim(B, H / P_h, W, C, P_h) --> dim(B, H / P_h, W / P_w, C, P_h, P_w)
+        patches_unfolded: torch.Tensor = h_patches_unfolded.unfold(2, self.patch_w, self.patch_w)
+
+        # Flatten each patch: dim(B, H / P_h, W / P_w, C, P_h, P_w) --> dim(B, H / P_h, W / P_w, C * P_h * P_w)
+        flattend_patches: torch.Tensor = patches_unfolded.flatten(3)
+
+        # Flatten patches: dim(B, H / P_h, W / P_w, C * P_h * P_w) --> dim(B, N, C * P_h * P_w), N = H * W / (P_h * P_w)
+        patches: torch.Tensor = flattend_patches.flatten(1, 2)  # BxNx(C * P_h * P_w)
 
         return patches
