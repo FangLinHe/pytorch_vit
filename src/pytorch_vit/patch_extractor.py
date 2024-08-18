ad61b4cffce51c3d4fc284c5ae9cc48314de4cf8
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-08-18 15:35:50 +0200

    Implement patch extractor

--- src/pytorch_vit/patch_extractor.py
+++ src/pytorch_vit/patch_extractor.py
@@ -5,8 +5,28 @@ from torch import nn
 
 
 class PatchExtractor(nn.Module):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, patch_h: int, patch_w: int) -> None:
+        super().__init__()
+        self.patch_h: int = patch_h
+        self.patch_w: int = patch_w
+        self.patch_len: int = patch_h * patch_w
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x
+        """Extracts patches from input image
+
+        Args:
+            x (torch.Tensor): Images have dimension BxCxHxW
+
+        Returns:
+            torch.Tensor: sequences of flattened 2D patches with dimension
+                BxNx(P_h * P_w * C), where N = H * W / (P_h * P_w)
+        """
+        axis_moved: torch.Tensor = x.moveaxis(1, 3)  # BCHW to BHWC
+        h_patches_unfolded: torch.Tensor = axis_moved.unfold(1, self.patch_h, self.patch_h)  # Bx(H/P_h)xWxCxP_h
+        patches_unfolded: torch.Tensor = h_patches_unfolded.unfold(
+            2, self.patch_w, self.patch_w
+        )  # Bx(H/P_h)x(W/P_w)xCxP_hxP_w
+        flattend_patches: torch.Tensor = patches_unfolded.flatten(3)  # Bx(H/P_h)x(W/P_w)x(C * P_h * P_w)
+        patches: torch.Tensor = flattend_patches.flatten(1, 2)  # Bx(H/P_h)x(W/P_w)x(C * P_h * P_w)
+
+        return patches
--- tests/test_patch_extractor.py
+++ tests/test_patch_extractor.py
@@ -1,8 +1,22 @@
 from __future__ import annotations
 
+import torch
+
 from pytorch_vit.patch_extractor import PatchExtractor
 
 
 def test_construct_patch_extractor():
-    extractor = PatchExtractor()
-    assert extractor is not None
+    batches: int = 2
+    channels: int = 3
+    image_height: int = 28
+    image_width: int = 32
+    patch_height: int = 4
+    patch_width: int = 8
+
+    extractor = PatchExtractor(patch_h=patch_height, patch_w=patch_width)
+    images: torch.Tensor = torch.rand((batches, channels, image_height, image_width))
+    patches: torch.Tensor = extractor(images)
+    expected_num_patches: int = image_height * image_width // patch_height // patch_width
+    exptected_patch_len: int = patch_height * patch_width * channels
+
+    assert patches.shape == (batches, expected_num_patches, exptected_patch_len)
