979bcdfd05e2fec77d118da315f2bfc0fbdceed6
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-08-18 23:45:44 +0200

    Add layer norm to transformer encoder

--- src/pytorch_vit/transformer_encoder.py
+++ src/pytorch_vit/transformer_encoder.py
@@ -5,8 +5,10 @@ from torch import nn
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, num_patches: int, embedding_dim: int) -> None:
+        super().__init__()
+        normalized_shape: tuple[int, int] = (1 + num_patches, embedding_dim)
+        self.layer_norm: nn.Module = nn.LayerNorm(normalized_shape)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x
+        return self.layer_norm(x)
--- tests/test_transformer_encoder.py
+++ tests/test_transformer_encoder.py
@@ -4,5 +4,5 @@ from pytorch_vit.transformer_encoder import TransformerEncoder
 
 
 def test_construct_transformer_encoder():
-    encoder = TransformerEncoder()
+    encoder = TransformerEncoder(16, 128)
     assert encoder is not None
