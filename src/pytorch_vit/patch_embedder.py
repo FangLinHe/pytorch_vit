Fix wrong shape of embeddings

src/pytorch_vit/patch_embedder.py
@@ -41,7 +41,7 @@ class PatchEmbedder(nn.Module):
 
         # BxD --> Bx1xD
         repeated_class_token: nn.Parameter = self.class_token.repeat(num_batches, 1).view(num_batches, 1, -1)
-        # concatenate Bx1xD and BxNxD --> Bx(1+N)xD --> Bx((1+N)*D)
-        embeddings: torch.Tensor = torch.cat((repeated_class_token, projected_embeddings), dim=1).view(num_batches, -1)
+        # concatenate Bx1xD and BxNxD --> Bx(1+N)xD
+        embeddings: torch.Tensor = torch.cat((repeated_class_token, projected_embeddings), dim=1)
 
         return embeddings

tests/test_patch_embedder.py
@@ -13,7 +13,5 @@ def test_patch_embedder():
     embedder = PatchEmbedder(patch_length, embedding_dim)
     patches: torch.Tensor = torch.rand((num_batches, num_patches, patch_length))
     embeddings: torch.Tensor = embedder(patches)
-    assert embeddings.shape == (num_batches, (1 + num_patches) * embedding_dim)
-
-    reshaped_embeddings: torch.Tensor = embeddings.view(num_batches, (1 + num_patches), embedding_dim)
-    assert (reshaped_embeddings[:, 0, :] == embedder.class_token).all()
+    assert embeddings.shape == (num_batches, 1 + num_patches, embedding_dim)
+    assert (embeddings[:, 0, :] == embedder.class_token).all()