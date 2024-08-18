8bf6434f51d380c41cb1c30a65c1bb4c5705085e
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-08-18 16:55:41 +0200

    Add named variables for better readability

--- tests/test_patch_extractor.py
+++ tests/test_patch_extractor.py
@@ -21,6 +21,7 @@ def test_extracted_patches_shape():
 
     assert patches.shape == (batches, expected_num_patches, exptected_patch_len)
 
+
 def test_extracted_patches_values():
     batches: int = 2
     image_height: int = 4
@@ -33,19 +34,28 @@ def test_extracted_patches_values():
     extractor = PatchExtractor(patch_h=patch_height, patch_w=patch_width)
     image_1: torch.Tensor = torch.arange(0, image_1_end).view((1, image_height, image_width))
     image_2: torch.Tensor = torch.arange(image_1_end, image_2_end).view((1, image_height, image_width))
-    images: torch.Tensor = torch.stack((image_1, image_2,), dim=0)
+    images: torch.Tensor = torch.stack(
+        (image_1, image_2),
+        dim=0,
+    )
     assert images.shape[0] == batches
     patches: torch.Tensor = extractor(images)
 
     assert patches.shape == (batches, image_height, image_width)
     expected_patches_1 = torch.Tensor(
         [
-            [0, 1, 2, 6, 7, 8,],
-            [3, 4, 5, 9, 10, 11,],
-            [12, 13, 14, 18, 19, 20,],
-            [15, 16, 17, 21, 22, 23,],
+            [0, 1, 2, 6, 7, 8],
+            [3, 4, 5, 9, 10, 11],
+            [12, 13, 14, 18, 19, 20],
+            [15, 16, 17, 21, 22, 23],
         ]
     ).type_as(patches)
-    expected_patches_2: torch.Tensor = expected_patches_1 + 24
-    expected_patches: torch.Tensor = torch.stack((expected_patches_1, expected_patches_2,), dim=0)
+    expected_patches_2: torch.Tensor = expected_patches_1 + image_1_end
+    expected_patches: torch.Tensor = torch.stack(
+        (
+            expected_patches_1,
+            expected_patches_2,
+        ),
+        dim=0,
+    )
     assert (patches == expected_patches).all()
