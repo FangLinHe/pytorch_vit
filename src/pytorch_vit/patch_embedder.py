76ad68f11e0c81b67d648935cc9cfbaab9f915c0
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-08-18 21:20:11 +0200

    Implement patch embedder

--- src/pytorch_vit/patch_embedder.py
+++ src/pytorch_vit/patch_embedder.py
@@ -1,12 +1,47 @@
 from __future__ import annotations
 
+from typing import Any
+
 import torch
 from torch import nn
 
 
 class PatchEmbedder(nn.Module):
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        patch_length: int,
+        embedding_dim: int,
+        device: str | None = None,
+        dtype: torch.dtype | None = None,
+    ) -> None:
+        super().__init__()
+        factory_kwargs: dict[str, Any] = {"device": device, "dtype": dtype}
+        self.patch_length: int = patch_length
+        self.embedding_dim: int = embedding_dim
+        self.class_token: nn.Parameter = nn.Parameter(
+            torch.empty(
+                (
+                    1,
+                    self.embedding_dim,
+                ),
+                **factory_kwargs,
+            ),
+            requires_grad=True,
+        )
+        self.linear_layer: nn.Module = nn.Linear(self.patch_length, self.embedding_dim, bias=False, **factory_kwargs)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x
+        nn.init.uniform_(self.class_token, a=-1.0, b=1.0)
+
+    def forward(self, patches: torch.Tensor) -> torch.Tensor:
+        num_batches, num_patches, patch_length = patches.shape
+        # BxNxP --> (B*N)xP
+        reshaped_patches: torch.Tensor = patches.view(-1, patch_length)
+        # (B*N)xD --> BxNxD
+        projected_embeddings: torch.Tensor = self.linear_layer(reshaped_patches).view(num_batches, num_patches, -1)
+
+        # BxD --> Bx1xD
+        repeated_class_token: nn.Parameter = self.class_token.repeat(num_batches, 1).view(num_batches, 1, -1)
+        # concatenate Bx1xD and BxNxD --> Bx(1+N)xD --> Bx((1+N)*D)
+        embeddings: torch.Tensor = torch.cat((repeated_class_token, projected_embeddings), dim=1).view(num_batches, -1)
+
+        return embeddings
--- tests/test_patch_embedder.py
+++ tests/test_patch_embedder.py
@@ -1,8 +1,19 @@
 from __future__ import annotations
 
+import torch
+
 from pytorch_vit.patch_embedder import PatchEmbedder
 
 
-def test_construct_patch_embedder():
-    embedder = PatchEmbedder()
-    assert embedder is not None
+def test_patch_embedder():
+    num_batches: int = 2
+    num_patches: int = 6
+    patch_length: int = 256
+    embedding_dim: int = 128
+    embedder = PatchEmbedder(patch_length, embedding_dim)
+    patches: torch.Tensor = torch.rand((num_batches, num_patches, patch_length))
+    embeddings: torch.Tensor = embedder(patches)
+    assert embeddings.shape == (num_batches, (1 + num_patches) * embedding_dim)
+
+    reshaped_embeddings: torch.Tensor = embeddings.view(num_batches, (1 + num_patches), embedding_dim)
+    assert (reshaped_embeddings[:, 0, :] == embedder.class_token).all()
